ba8a40c643d5da45b23bb16b99f0852f7db8b254
G.Hemanth Sai <73033596+HemanthSai7@users.noreply.github.com>
2022-03-04 19:48:48 +0530

    "final tested code"

--- ModelName.py
+++ ModelName.py
@@ -1,23 +1,18 @@
 from abc import ABC, abstractmethod
 import tensorflow as tf
-
-
 class ModelName(ABC):
+    def __init__(self):
+        __model_path = None
+        __input_shape = None
 
-    __model_path = None
-    __input_shape = None
-
-    def __int__(self):
+    @staticmethod
+    @abstractmethod
+    def load_model(self):
         pass
 
     @staticmethod
     @abstractmethod
-    def load_model():
-        pass
-
-    @staticmethod
-    @abstractmethod
-    def preprocessing(image):
+    def preprocessing(self,image):
         image = tf.io.decode_image(image)
         image = tf.expand_dims(image, 0)
         return image
